a6bf7398badb9fa123743e2f3968977e7b0eee4b
iatagun <47903385+iatagun@users.noreply.github.com>
2024-10-10 01:26:01 +0300

    text generation
    dsadsada

--- models/text_generation.py
+++ models/text_generation.py
@@ -2,6 +2,11 @@ import numpy as np
 import tensorflow as tf
 from difflib import SequenceMatcher
 import random
+from collections import defaultdict
+import nltk
+from nltk import word_tokenize
+from nltk import trigrams
+from nltk import bigrams
 
 class SentenceGenerator:
     def __init__(self, text, transition_model_path, seq_length=15):
@@ -10,18 +15,22 @@ class SentenceGenerator:
         self.tokenizer = tf.keras.preprocessing.text.Tokenizer()
         self.tokenizer.fit_on_texts(self.sentences)
         self.seq_length = seq_length
+        
+        # Initialize models for bigram and trigram
+        self.model_bigram = defaultdict(lambda: defaultdict(lambda: 0))
+        self.model_trigram = defaultdict(lambda: defaultdict(lambda: 0))
+        
+        # Build the bigram and trigram models
+        self._build_ngram_models(text)
 
     def _split_into_sentences(self, text):
-        # Metni cümlelere ayırır
         return [sentence.strip() for sentence in text.split('.') if sentence]
 
     def are_sentences_similar(self, sentence1, sentence2, threshold=0.8):
-        # İki cümlenin benzerliğini kontrol eder
         similarity = SequenceMatcher(None, sentence1, sentence2).ratio()
         return similarity > threshold
 
     def predict_transition(self, current_sentence, new_sentence):
-        # Cümlelerin geçiş türünü tahmin eder
         current_sequence = self.tokenize(current_sentence)
         new_sequence = self.tokenize(new_sentence)
 
@@ -34,12 +43,59 @@ class SentenceGenerator:
         return np.argmax(prediction, axis=1)
 
     def tokenize(self, sentence):
-        # Cümleyi tokenize eder
         return [self.tokenizer.word_index[word] for word in sentence.split() if word in self.tokenizer.word_index]
 
-    def generate_sentence(self):
-        # Rastgele bir cümle seçer
-        return random.choice(self.sentences)
+    def _build_ngram_models(self, text):
+        # Tokenize the text
+        tokenized_text = [list(map(str.lower, word_tokenize(sent))) for sent in nltk.sent_tokenize(text)]
+
+        # Build bigram model
+        for sentence in tokenized_text:
+            for w1, w2 in bigrams(sentence, pad_right=True, pad_left=True):
+                self.model_bigram[w1][w2] += 1
+
+        # Transform counts to probabilities for bigrams
+        for w1 in self.model_bigram:
+            total_count = float(sum(self.model_bigram[w1].values()))
+            for w2 in self.model_bigram[w1]:
+                self.model_bigram[w1][w2] /= total_count
+
+        # Build trigram model
+        for sentence in tokenized_text:
+            for w1, w2, w3 in trigrams(sentence, pad_right=True, pad_left=True):
+                self.model_trigram[(w1, w2)][w3] += 1
+
+        # Transform counts to probabilities for trigrams
+        for w1_w2 in self.model_trigram:
+            total_count = float(sum(self.model_trigram[w1_w2].values()))
+            for w3 in self.model_trigram[w1_w2]:
+                self.model_trigram[w1_w2][w3] /= total_count
+
+    def generate_sentence(self, length=10, prob_threshold=0.05):
+        start_words = random.choice(list(self.model_trigram.keys()))
+        sentence = list(start_words)
+
+        for _ in range(length - 2):
+            next_word_probs = self.model_trigram.get((sentence[-2], sentence[-1]), {})
+            if next_word_probs:
+                filtered_probs = {word: prob for word, prob in next_word_probs.items() if prob > prob_threshold}
+                if filtered_probs:
+                    next_word = random.choices(list(filtered_probs.keys()), weights=list(filtered_probs.values()))[0]
+                    if next_word: 
+                        sentence.append(next_word)
+                    else:
+                        break  # Stop if next word is None
+                else:
+                    break  # Stop if no words meet the probability threshold
+            else:
+                break  # Stop if no prediction for the next word
+        
+        # Ensure no None values in sentence
+        sentence = [word for word in sentence if word is not None]
+
+        return ' '.join(sentence)
+
+
 
     def generate_text(self, initial_sentence, num_sentences):
         generated_text = initial_sentence
@@ -68,7 +124,6 @@ def load_text_data(file_path):
 
 # Örnek kullanım
 if __name__ == "__main__":
-    # Metin dosyasının yolunu belirtin
     text_file_path = "C:\\Users\\user\\OneDrive\\Belgeler\\GitHub\\Lgram\\models\\text_gen_data.txt"
     transition_model_path = "C:\\Users\\user\\OneDrive\\Belgeler\\GitHub\\Lgram\\models\\best_transition_model.keras"
     
